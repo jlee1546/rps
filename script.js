8c0bdfe5d4ac8b5d7df33dafc31ce4587e6e36f5
Jonathan Lee <jlee1546.jl@gmail.com>
2022-06-22 00:39:41 -0400

    Complete script file with addition of main game function to control game play

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
+let playerNumberWins = 0;
+let computerNumberWins = 0;
+let numberOfTie = 0;
+
 let computerChoice = () => {
   let randomNumber = Math.random() * 3;
   if (randomNumber <= 1) {
-    console.log(randomNumber);
     return "Rock".toLowerCase();
   } else if (randomNumber > 1 && randomNumber < 2) {
-    console.log(randomNumber);
     return "Paper".toLowerCase();
   } else {
-    console.log(randomNumber);
     return "Scissors".toLowerCase();
   }
 };
@@ -27,26 +28,58 @@ let playRound = () => {
   if (playerSelection === "rock") {
     if (computerSelection === "paper") {
       console.log("Computer Wins!");
+      computerNumberWins++;
     } else if (computerSelection === "scissors") {
       console.log("Player Wins!");
+      playerNumberWins++;
     } else {
       console.log("It's a Tie!");
+      numberOfTie++;
     }
   } else if (playerSelection === "paper") {
     if (computerSelection === "rock") {
       console.log("Player Wins!");
+      playerNumberWins++;
     } else if (computerSelection === "scissors") {
       console.log("Computer Wins");
+      computerNumberWins++;
     } else {
       console.log("It's a Tie!");
+      numberOfTie++;
     }
-  } else {
+  } else if (playerSelection === "scissors") {
     if (computerSelection === "rock") {
       console.log("Computer Wins!");
+      computerNumberWins++;
     } else if (computerSelection === "paper") {
       console.log("Player Wins");
+      playerNumberWins++;
     } else {
       console.log("It's a Tie!");
+      numberOfTie++;
     }
+  } else {
+    console.log("Cannot be determined!");
   }
+};
+
+let displayOutcome = () => {
+  return `
+  Player Score : ${playerNumberWins}
+  Computer Score : ${computerNumberWins}
+  Tie Score : ${numberOfTie}`;
+};
+
+let resetScore = () => {
+  playerNumberWins = 0;
+  computerNumberWins = 0;
+  numberOfTie = 0;
+};
+
+let game = () => {
+  for (let i = 1; i <= 5; i++) {
+    playRound();
+  }
+  console.log(displayOutcome());
+  resetScore();
 };
