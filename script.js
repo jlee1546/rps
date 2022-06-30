2a2d6d0f0175f089b8a083a8723790737eca4dc0
Jonathan Lee <jlee1546.jl@gmail.com>
2022-06-30 15:06:26 -0400

    Stuck at changeMessage logic

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ selections.forEach((selection) =>
   selection.addEventListener("click", function (e) {
     const player = e.target.getAttribute("id");
     const computer = computerChoice();
-    playRound(player, computer);
-    changeMessage(player.toUpperCase(), computer.toUpperCase());
+    const round = playRound(player, computer);
+    changeMessage(player.toUpperCase(), computer.toUpperCase(), round);
   })
 );
 
@@ -25,54 +25,50 @@ let computerChoice = () => {
   }
 };
 
-// Function that allows the player to choose
-// let playerChoice = () => {
-
-// };
-
 // Funtions that changes the message displayed
-let changeMessage = (player, computer) => {
+let changeMessage = (player, computer, round) => {
   const message = document.querySelector(".message");
   message.textContent = `Player chooses ${player} and Computer chooses ${computer}`;
+  setTimeout((message.textContent = round), 10000);
 };
 
 // Funtion that plays a round, modifies global score variable of outcome
 let playRound = (playerSelection, computerSelection) => {
   if (playerSelection === "rock") {
     if (computerSelection === "paper") {
-      console.log("Computer Wins!");
+      return "Computer Wins!";
       computerNumberWins++;
     } else if (computerSelection === "scissors") {
-      console.log("Player Wins!");
+      return "Player Wins!";
       playerNumberWins++;
     } else {
-      console.log("It's a Tie!");
+      return "It's a Tie!";
       numberOfTie++;
     }
   } else if (playerSelection === "paper") {
     if (computerSelection === "rock") {
-      console.log("Player Wins!");
+      return "Player Wins!";
       playerNumberWins++;
     } else if (computerSelection === "scissors") {
-      console.log("Computer Wins");
+      return "Computer Wins";
       computerNumberWins++;
     } else {
-      console.log("It's a Tie!");
+      return "It's a Tie!";
       numberOfTie++;
     }
   } else if (playerSelection === "scissors") {
     if (computerSelection === "rock") {
-      console.log("Computer Wins!");
+      return "Computer Wins!";
       computerNumberWins++;
     } else if (computerSelection === "paper") {
-      console.log("Player Wins");
+      return "Player Wins";
       playerNumberWins++;
     } else {
-      console.log("It's a Tie!");
+      return "It's a Tie!";
       numberOfTie++;
     }
   } else {
-    console.log("Cannot be determined!");
+    return "Cannot be determined!";
   }
 };
 
