4ac75ce7b5106fec49bc7fbec286594a053b8da8
Jonathan Lee <jlee1546.jl@gmail.com>
2022-07-01 13:40:52 -0400

    Add scoreBoard update and winner decision logic

--- script.js
+++ script.js
@@ -2,19 +2,23 @@
 let playerNumberWins = 0;
 let computerNumberWins = 0;
 let numberOfTie = 0;
+let roundNumber = 1;
 
-let selections = document.querySelectorAll(".selections");
+const selections = document.querySelectorAll(".selections");
 selections.forEach((selection) =>
   selection.addEventListener("click", function (e) {
     const player = e.target.getAttribute("id");
     const computer = computerChoice();
     const round = playRound(player, computer);
     changeMessage(player.toUpperCase(), computer.toUpperCase(), round);
+    updateScoreBoard();
+    incrementRoundNUmber();
+    checkForWinner();
   })
 );
 
 // Function that allows computer to choose
-let computerChoice = () => {
+const computerChoice = () => {
   let randomNumber = Math.random() * 3;
   if (randomNumber <= 1) {
     return "Rock".toLowerCase();
@@ -26,46 +30,46 @@ let computerChoice = () => {
 };
 
 // Funtions that changes the message displayed
-let changeMessage = (player, computer, round) => {
+const changeMessage = (player, computer, round) => {
   const message = document.querySelector(".message");
-  message.textContent = `Player chooses ${player} and Computer chooses ${computer}`;
-  setTimeout((message.textContent = round), 10000);
+  message.textContent = `Player chooses ${player} and Computer chooses ${computer}! 
+  ${round}`;
 };
 
 // Funtion that plays a round, modifies global score variable of outcome
-let playRound = (playerSelection, computerSelection) => {
+const playRound = (playerSelection, computerSelection) => {
   if (playerSelection === "rock") {
     if (computerSelection === "paper") {
-      return "Computer Wins!";
       computerNumberWins++;
+      return "Computer Wins!";
     } else if (computerSelection === "scissors") {
-      return "Player Wins!";
       playerNumberWins++;
+      return "Player Wins!";
     } else {
-      return "It's a Tie!";
       numberOfTie++;
+      return "It's a Tie!";
     }
   } else if (playerSelection === "paper") {
     if (computerSelection === "rock") {
-      return "Player Wins!";
       playerNumberWins++;
+      return "Player Wins!";
     } else if (computerSelection === "scissors") {
-      return "Computer Wins";
       computerNumberWins++;
+      return "Computer Wins";
     } else {
-      return "It's a Tie!";
       numberOfTie++;
+      return "It's a Tie!";
     }
   } else if (playerSelection === "scissors") {
     if (computerSelection === "rock") {
-      return "Computer Wins!";
       computerNumberWins++;
+      return "Computer Wins!";
     } else if (computerSelection === "paper") {
-      return "Player Wins";
       playerNumberWins++;
+      return "Player Wins";
     } else {
-      return "It's a Tie!";
       numberOfTie++;
+      return "It's a Tie!";
     }
   } else {
     return "Cannot be determined!";
@@ -73,13 +77,57 @@ let playRound = (playerSelection, computerSelection) => {
 };
 
 // Funtion that displays the current score
-let displayOutcome = () => {};
+const updateScoreBoard = () => {
+  const playerScore = document.querySelector(".player .display1");
+  const computerScore = document.querySelector(".computer .display1");
+  const round = document.querySelector(".round .display2");
+  const tie = document.querySelector(".tie .display2");
+
+  playerScore.textContent = playerNumberWins;
+  computerScore.textContent = computerNumberWins;
+  round.textContent = roundNumber;
+  tie.textContent = numberOfTie;
+};
+
+//Function to increment the round number
+const incrementRoundNUmber = () => {
+  roundNumber += 1;
+};
+
+//Function that changes display message after a winner is determined
+const declareWinner = (winner) => {
+  const message = document.querySelector(".message");
+  if (winner === "player") {
+    message.textContent = "Player Wins!";
+  } else {
+    message.textContent = "Computer Wins!";
+  }
+};
+
+//Function to check for winner
+const checkForWinner = () => {
+  const playerScore = playerNumberWins;
+  const computerScore = computerNumberWins;
+
+  if (playerScore === 5) {
+    declareWinner("player");
+    resetScore();
+    updateScoreBoard();
+  } else if (computerScore === 5) {
+    declareWinner("computer");
+    updateScoreBoard();
+    resetScore();
+  } else {
+    return;
+  }
+};
 
 // Function used to reset the global score variables for a new game
-let resetScore = () => {
+const resetScore = () => {
   playerNumberWins = 0;
   computerNumberWins = 0;
   numberOfTie = 0;
+  roundNumber = 1;
 };
 
 //Function that plays an entire game
